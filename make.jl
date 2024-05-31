b723850a8c72b7575e93fdc0b1da2b8b0f8a9564
CWillberg <christian.willberg@gmx.de>
2024-05-31 10:19:30 +0200

    first commit

---
+++ make.jl
@@ -0,0 +1,29 @@
+using Documenter, DocumenterCitations, DocumenterMermaid
+
+bib = CitationBibliography(joinpath(@__DIR__, "literatur", "literatur.bib"))
+
+makedocs(
+    plugins=[bib],
+    authors="Christian Willberg <christian.willberg@h2.de>",
+    doctest=true,
+    checkdocs=:none, # :all, :exports, :none
+    sitename="Materialbibliothek",
+    repo=Documenter.Remotes.GitHub("CWillberg", "Materialbibliothek"), format=Documenter.HTML(
+        canonical="https://github.com/CWillberg/Materialbibliothek.git",
+        assets=["assets/favicon.ico"],
+        edit_link="main"
+    ),
+    pages=Any[
+        "Materialbibliothek"=>"index.md",
+        "Metalle"=>Any[
+            "Eisen"=>"metalle/eisen.md"
+        ],
+        "Kunststoffe"=>Any["Eisen"=>"kunststoffe/PMMA.md"
+        ],
+        "Naturwerkstoffe"=>Any["Holz"=>"naturwerkstoffe/holz.md"
+        ]
+    ]
+)
+deploydocs(
+    repo="https://github.com/CWillberg/Materialbibliothek.git",
+)
---
+++ src/index.md
@@ -0,0 +1,3 @@
+# Materialbibliothek
+Haus 8 Raum 1.06
+![](figures/material_bibliothek.png)
