c78c8cd3fc699a4f09514d1399fb0a5deaaa71fd
CWillberg <christian.willberg@gmx.de>
2024-06-13 17:17:20 +0200

    update

--- make.jl
+++ make.jl
@@ -15,12 +15,18 @@ makedocs(
     ),
     pages=Any[
         "Übersicht"=>"index.md",
+        "Gläser"=>Any["PMMA"=>"glaeser/obsidian.md"
+        ],
+        "Kunststoffe"=>Any["PMMA"=>"kunststoffe/PMMA.md"
+        ],
         "Metalle"=>Any[
             "Eisen"=>"metalle/eisen.md",
             "Mangan"=>"metalle/mangan.md",
-            "Stahl"=>"metalle/stahl.md"
-        ],
-        "Kunststoffe"=>Any["PMMA"=>"kunststoffe/PMMA.md"
+            "Stahl"=>"metalle/stahl.md",
+            "Stahl"=>"metalle/aluminium.md",
+            "Stahl"=>"metalle/bronze.md",
+            "Stahl"=>"metalle/wolfram.md",
+            "Stahl"=>"metalle/paladium.md"
         ],
         "Naturwerkstoffe"=>Any["Holz"=>"naturwerkstoffe/holz.md"
         ]
